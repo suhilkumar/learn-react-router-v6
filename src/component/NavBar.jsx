df8a1aeeb0bd3e6f50f86da10b1ca923de112e28
Suhilkumar <suhilkumarpatel@gmail.com>
2022-07-30 14:25:19 -0500

    Show which link is active and highlight it yellow

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,17 +1,25 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 const NavBar = () => {
-  return (
-    <>
-        <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">about</Link></li>
-            <li><Link to="/contact">contact</Link></li>
-            <li><Link to="/custom">custom</Link></li>
-        </ul>
-    </>
-  )
+    return (
+        <>
+            <ul>
+                <li><NavLink to="/" style={({ isActive }) => {
+                    return { backgroundColor: isActive ? "yellow" : "" }
+                }}>Home</NavLink></li>
+                <li><NavLink to="/about" style={({ isActive }) => {
+                    return { backgroundColor: isActive ? "yellow" : "" }
+                }}>about</NavLink></li>
+                <li><NavLink to="/contact" style={({ isActive }) => {
+                    return { backgroundColor: isActive ? "yellow" : "" }
+                }}>contact</NavLink></li>
+                <li><NavLink to="/custom" style={({ isActive }) => {
+                    return { backgroundColor: isActive ? "yellow" : "" }
+                }}>custom</NavLink></li>
+            </ul>
+        </>
+    )
 }
 
 export default NavBar
