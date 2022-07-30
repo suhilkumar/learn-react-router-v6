39abb30e1af184e0b661648ffebb57dce0bd387b
Suhilkumar <suhilkumarpatel@gmail.com>
2022-07-30 13:05:38 -0500

    create navigation bar to access all routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,18 @@ import { BrowserRouter,Routes, Route } from "react-router-dom";
 import Home from './component/pages/Home'
 import About from './component/pages/About'
 import Contact from './component/pages/contact'
+import NavBar from "./component/NavBar";
 export default () => {
   return (
     <div>
       <h2>Learn Router</h2>
+      <NavBar/>
       <BrowserRouter>
         <Routes>
           <Route path='/' element={<Home/>} />
           <Route path='/about' element={<About/>} />
           <Route path='/contact' element={<Contact/>} />
-          <Route path='/suhil' element={<h1>this is Suhil</h1>}/>
+          <Route path='/custom' element={<><h1>this is Suhil</h1><h2>This is a custom html</h2></>}/>
 
         </Routes>
       </BrowserRouter>
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">about</a></li>
+            <li><a href="/contact">contact</a></li>
+            <li><a href="/custom">custom</a></li>
+        </ul>
+    </>
+  )
+}
+
+export default NavBar
