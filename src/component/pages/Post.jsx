30e6a3359b8efef1833f58b66635c48e54db0bb7
Suhilkumar <suhilkumarpatel@gmail.com>
2022-07-31 09:05:20 -0500

    useSearchParams

--- src/component/pages/Post.jsx
+++ src/component/pages/Post.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useSearchParams } from 'react-router-dom'
 const Post = () => {
     const {category,id} = useParams();
+
+    const [searchParams, setSearchParams] = useSearchParams();
+    console.log(' the url is localhost:3000/post/laptop/123?price=20')
+    console.log(searchParams.get('price'))
+    
+    console.log(` lets say u have multiple query :
+    // the url is localhost:3000/post/laptop/123?price=20&sort=asc`)
+    console.log(searchParams.get('price'))
+    console.log(searchParams.get('sort'))
+    
 
   return (
     <div>
