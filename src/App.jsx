0a0105458fc06d964b029adecf27096f8c50512d
Suhilkumar <suhilkumarpatel@gmail.com>
2022-08-01 07:24:56 -0500

    create-login template

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter,Routes, Route } from "react-router-dom";
+import { BrowserRouter,Routes, Route, Navigate } from "react-router-dom";
+
+// import custom components
+import NavBar from "./component/NavBar";
+
 import Home from './component/pages/Home'
 import About from './component/pages/About'
 import Contact from './component/pages/contact'
-import NavBar from "./component/NavBar";
 import Post from "./component/pages/Post";
+import Login from "./component/pages/login";
+import Dashboard from "./component/pages/dashboard";
+
 export default () => {
+  let isLogged = true;
+
   return (
     <div>
       <h2>Learn Router</h2>
@@ -17,10 +25,27 @@ export default () => {
           <Route path='/contact' element={<Contact/>} />
           <Route path='/post/:category' element={<Post/>} />
           <Route path='/post/:category/:id' element={<Post/>} />
-          <Route path='/custom' element={<><h1>this is Suhil</h1><h2>This is a custom html</h2></>}/>
+          <Route path='/custom' element={
+                                  <>
+                                    <h1>this is Suhil</h1>
+                                   <h2>This is a custom html</h2>
+                                  </>}/>
           <Route path='*' element={<><h1>Error: 404</h1> <h3>Page Not Found</h3></>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/dash" element={isLogged ? <Dashboard/> : <Navigate to='/login' replace />}/>
         </Routes>
       </BrowserRouter>
     </div>
   )
 }
+
+
+
+
+
+
+
+
+
+
+
